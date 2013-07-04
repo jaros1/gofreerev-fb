f39238d2c114cdc34a583a13f1f1c73e80c16219
jaros1 <jan.roslind.dk@gmail.com>
2013-07-04 10:13:09 +0200

    New empty rails 4.0 project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GofreerevFb::Application.config.secret_key_base = '234502d24d620b3eb5f9fe7f509060978d2b6e901c121a42fe5ead7a0e7a67141bbc6ea31092d6111c087d7505f3efc57331156cde6ff526b27c785b30b72c72'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GofreerevFb::Application.config.session_store :cookie_store, key: '_gofreerev-fb_session'
