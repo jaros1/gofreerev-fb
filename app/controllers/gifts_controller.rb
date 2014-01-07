93bcbc2b15f939456875cbf6d347ec509361d35f
jaros1 <jan.roslind.dk@gmail.com>
2014-01-07 19:14:48 +0100

    adding custom OG description for linkedin

--- app/controllers/gifts_controller.rb
+++ app/controllers/gifts_controller.rb
@@ -367,7 +367,7 @@ class GiftsController < ApplicationController
       title = text
       on_error_desc = "Help each other and the environment. Share your resources. #{APP_NAME} is a play with some concepts (gift network, free money and negative interest) from Charles Eisensteins book Sacred Economics."
       begin
-        description = t '.og_def_desc_#{api_gift.provider}', :appname => APP_NAME, :api_name => provider_downcase(api_gift.provider), :raise => I18n::MissingTranslationData
+        description = t ".og_def_desc_#{api_gift.provider}", :appname => APP_NAME, :api_name => provider_downcase(api_gift.provider), :raise => I18n::MissingTranslationData
       rescue I18n::MissingTranslationData => e
         logger.error2 "Error in translate key gifts.show.og_desc_#{api_gift.provider}"
         logger.error2 "#{e.message} (#{e.class})"
